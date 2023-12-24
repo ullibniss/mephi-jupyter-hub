9d934a39384e623a7fb6055066e2246d32f087d4
Your Name <you@example.com>
2023-12-24 14:58:12 +0300

    Added jupyterhub role

--- jupyter/jupyter_hub/ansible/roles/jupyterhub/templates/jupyterhub_config.py
+++ jupyter/jupyter_hub/ansible/roles/jupyterhub/templates/jupyterhub_config.py
@@ -1,3 +1,14 @@
 import os
+from dockerspawner import SwarmSpawner
 
 c = get_config()
+c.JupyterHub.spawner_class = SwarmSpawner
+c.JupyterHub.confirm_no_ssl = True
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.SwarmSpawner.use_internal_ip = True
+#c.SwarmSpawner.image = 'zonca/jupyterhub-datascience-systemuser'
+c.SwarmSpawner.image = 'quay.io/jupyter/tensorflow-notebook'
+c.SwarmSpawner.network_name = "jupyterhub_jupyternet"
+c.SwarmSpawner.extra_host_config = {'network_mode': c.SwarmSpawner.network_name}
+c.SwarmSpawner.start_timeout = 300
+c.SwarmSpawner.hub_ip_connect = "10.0.4.1"
